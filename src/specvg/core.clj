3a5b57cfd4657cd3ac54dadcecf3ba9856414af4
whybin <31753349+whybin@users.noreply.github.com>
2018-01-12 16:33:21 -0500

    Refactor tree generation

--- src/specvg/core.clj
+++ src/specvg/core.clj
@@ -16,35 +16,41 @@
   (utils/rand-bias (if can-terminate choices
                      (dissoc choices :terminate))))
 
-(defn grow-branch
-  "Generates a series of branched paths
-  Params:
-    path : vector
-    continue-for : number, depth to disallow termination for
-    branches : path vector, accumulation of paths
-  Return: vector, of paths."
-  [path continue-for branches]
-  (let [can-terminate (= continue-for 0)
-        continue-for (if (> continue-for 0) (- continue-for 1) 0)]
-    (case (make-choice can-terminate)
-      :terminate (conj branches path)
-      :branch (recur (conj path :l [unit unit])
-                     continue-for
-                     (grow-branch (conj path :l [(* unit -1) unit])
-                                  continue-for branches))
-      :extend (recur (conj path :l [0 unit]) continue-for branches))))
+(defn grow-tree
+  [path center continue-for]
+  (letfn [(grow-branch
+            ; Generates a series of branched paths
+            ; Params:
+            ;   path : vector
+            ;   continue-for : number, depth to disallow termination for
+            ;   branches : path vector, accumulation of paths
+            ; Return: vector, of paths.
+            [path continue-for branches]
+            (let [can-terminate (= continue-for 0)
+                  continue-for (if (> continue-for 0) (- continue-for 1) 0)]
+              (case (make-choice can-terminate)
+                :terminate (conj branches path)
+                :branch (recur (conj path :l [unit unit])
+                               continue-for
+                               (grow-branch (conj path :l [(* unit -1) unit])
+                                            continue-for branches))
+                :extend (recur (conj path :l [0 unit]) continue-for branches))))]
+    (grow-branch path continue-for [])))
 
 (defn create-tree
   "Generates a branched tree SVG
   Params:
     attrs : map, SVG attributes
   Return: vector, a Dali document"
-  [attrs]
-  (into [:dali/page] (grow-branch [:path attrs :M [200 0]] 15 [])))
+  [attrs center-radius]
+  (let [center [200 0]
+        origin [200 center-radius]]
+    (into [:dali/page]
+          (grow-tree [:path attrs :M origin] center 15))))
 
 (defn -main
   [& args]
   (io/render-png (create-tree {:stroke :indigo
                                :stroke-width 4
-                               :fill :none}) "out/test.png")
+                               :fill :none} 12) "out/test.png")
   (println "done!"))
